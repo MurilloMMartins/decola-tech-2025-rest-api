61c88537850cdf6afeb5c26fdce22312a9240b9e
MurilloMMartins <murillomartins19@gmail.com>
2025-03-09 16:27:21 -0300

    Initial configuration

---
+++ src/test/java/mmartins/murillo/DecolaTech2025RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mmartins.murillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTech2025RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
