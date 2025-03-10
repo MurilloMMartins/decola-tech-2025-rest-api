3333a55c432dd1cc1ad0c303f4b645ceb0ad38eb
MurilloMMartins <murillomartins19@gmail.com>
2025-03-10 16:02:38 -0300

    Bugfix: forgot to import User class

--- src/main/java/mmartins/murillo/domain/repository/UserRepository.java
+++ src/main/java/mmartins/murillo/domain/repository/UserRepository.java
@@ -3,6 +3,8 @@ package mmartins.murillo.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import mmartins.murillo.domain.model.User;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
