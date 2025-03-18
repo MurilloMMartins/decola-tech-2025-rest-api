7b6c17169ea586bb7b232932a11b40517dc3aa55
MurilloMMartins <murillomartins19@gmail.com>
2025-03-18 16:50:15 -0300

    Removed unnecessary import

--- src/main/java/mmartins/murillo/domain/model/User.java
+++ src/main/java/mmartins/murillo/domain/model/User.java
@@ -2,7 +2,6 @@ package mmartins.murillo.domain.model;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
