d988f78bb97ad9fb6693dd517fee40e7c6a67050
MurilloMMartins <murillomartins19@gmail.com>
2025-03-21 15:38:11 -0300

    Removed unnecessary import

--- src/main/java/mmartins/murillo/domain/model/User.java
+++ src/main/java/mmartins/murillo/domain/model/User.java
@@ -1,14 +1,10 @@
 package mmartins.murillo.domain.model;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 
 @Entity(name = "tb_user")
 public class User {
